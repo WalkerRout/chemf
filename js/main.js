cd0a86120a00c7d89d9ed1be0a597aaa036229b1
WalkerRout <walkerrout04@gmail.com>
2022-01-21 16:57:56 -0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function navigation(){
+    const navToggle = document.querySelector(".nav-toggle");
+    const navLinks = document.querySelectorAll(".nav__link");
+
+    navToggle.addEventListener("click", () => {
+        document.body.classList.toggle("nav-open");
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            document.body.classList.remove("nav-open");
+        })
+    });
+}
+
+
+window.onload = function() {
+    navigation();
+}
